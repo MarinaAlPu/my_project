eaba4f3d202e4743da016b9b04b29d512f6e3e95
MarinaAlPu <marina.pudovkina13@gmail.com>
2023-07-17 02:48:20 +0500

    Modify funcs.py

--- funcs.py
+++ funcs.py
@@ -2,4 +2,9 @@ def sum_func(a: int, b: int) -> int:
     return a + b
 
 
-print(sum(3, 759))
+def sub_func(c: int, f: int) -> int:
+    return c + f
+
+
+print(sum_func(3, 759))
+print(sub_func(13, 0))
