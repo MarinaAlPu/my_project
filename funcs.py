443c0445bf6e9ddb654ce43525e38a89a3701143
MarinaAlPu <marina.pudovkina13@gmail.com>
2023-07-17 02:52:43 +0500

    Modify main.py

--- funcs.py
+++ funcs.py
@@ -4,7 +4,3 @@ def sum_func(a: int, b: int) -> int:
 
 def sub_func(c: int, f: int) -> int:
     return c + f
-
-
-# print(sum_func(3, 759))
-# print(sub_func(13, 0))
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 from funcs import sum_func
+from funcs import sub_func
 
 sum_func(7, 54)
+sub_func(86, 93)
